ec5dcb3e15717430741d7543794a323f024a571e
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-03-08 20:25:55 -0800

    add docstring

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ rm -f proto/client/* proto/server/* \
     frontend/html/frontend.js frontend/html/frontend.js.map
 
 echo "generating code from proto files..."
+# the /tmp path is for CI only
 protoc -I. -I/tmp/protobuf/include -Ivendor/ ./proto/web.proto \
     --gopherjs_out=plugins=grpc:${GOPATH}/src \
     --go_out=plugins=grpc:$GOPATH/src
