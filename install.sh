9e2d125a37c4095102924b85d5360d55fd56754d
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-08-17 15:10:21 -0700

    Remove gopherjs from the install script. Refs #5

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# install 
-go install ./vendor/github.com/golang/protobuf/protoc-gen-go \
-    ./vendor/github.com/johanbrandhorst/protobuf/protoc-gen-gopherjs
+# install protoc-gen-go from vendor folder
+go install ./vendor/github.com/golang/protobuf/protoc-gen-go 
 
-# GopherJS cannot be vendored so must be fetched
-go get -u github.com/gopherjs/gopherjs
