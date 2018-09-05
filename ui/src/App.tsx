86d9354c18836f46ba15a377fb1a7adee36bf938
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-09-04 21:41:50 -0700

    Get rid of the right sidebar.
    This isn't used in our design. Adjust CSS grid template for the desktop
    view, since that sidebar is no longer a column in the grid.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -39,7 +39,7 @@ class App extends React.Component {
   public render() {
     return (
       <div className="container">
-        <header>- co-chair -</header>
+        <header>co-chair</header>
         <nav>
           <div><a href="/login">login!!!!</a></div>
           <Button></Button>
@@ -52,7 +52,6 @@ class App extends React.Component {
           <p>Vestibulum consectetur sit amet nisi ut consectetur. Praesent efficitur, nibh vitae fringilla scelerisque, est neque faucibus quam, in iaculis purus libero eget mauris. Curabitur et luctus sapien, ac gravida orci. Aliquam erat volutpat. In hac habitasse platea dictumst. Aenean commodo, arcu a commodo efficitur, libero dolor mollis turpis, non posuere orci leo eget enim. Curabitur sit amet elementum orci, pulvinar dignissim urna. Morbi id ex eu ex congue laoreet. Aenean tincidunt dolor justo, semper pretium libero luctus nec. Ut vulputate metus accumsan leo imperdiet tincidunt. Phasellus nec rutrum dolor. Cras imperdiet sollicitudin arcu, id interdum nibh fermentum in.</p>
           <p>Vestibulum consectetur sit amet nisi ut consectetur. Praesent efficitur, nibh vitae fringilla scelerisque, est neque faucibus quam, in iaculis purus libero eget mauris. Curabitur et luctus sapien, ac gravida orci. Aliquam erat volutpat. In hac habitasse platea dictumst. Aenean commodo, arcu a commodo efficitur, libero dolor mollis turpis, non posuere orci leo eget enim. Curabitur sit amet elementum orci, pulvinar dignissim urna. Morbi id ex eu ex congue laoreet. Aenean tincidunt dolor justo, semper pretium libero luctus nec. Ut vulputate metus accumsan leo imperdiet tincidunt. Phasellus nec rutrum dolor. Cras imperdiet sollicitudin arcu, id interdum nibh fermentum in.</p>
         </main>
-        <aside>Related links</aside>
         <footer>Footer</footer>
       </div>
     );
